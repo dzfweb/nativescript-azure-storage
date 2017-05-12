785c02306010e5a9de7e0783d7018cc47fef8d2e
Douglas Zanco Franco <douglas@vizir.com.br>
2017-05-12 16:25:19 -0300

    deleted unusable 'common file' and update package version

--- nativescript-azure-storage.common.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class Common {
-    static connectionString: string;
-}
--- nativescript-azure-storage.common.ts
+++
@@ -1,7 +0,0 @@
-import * as app from 'application';
-
-export class Common {
-  public static connectionString: string;
-}
-
-
