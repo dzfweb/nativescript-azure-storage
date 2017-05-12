9b2e330eeab819ddde75cea44cafe83a9f54a658
Douglas Zanco Franco <douglas@vizir.com.br>
2017-05-12 15:55:08 -0300

    update references

--- index.d.ts
+++ index.d.ts
@@ -4,8 +4,9 @@ export declare class NativescriptAzureStorage {
   // use take the ios or android .d.ts files and copy/paste them here
   connectionString: string;
   constructor(connectionString: string);
-
+  createTable(tableName: string): Promise<any>;
   addRows(table: string, partitionKey: string, rowKeyName: string, items: any[]): Promise<any>;
   addRow(table: string, itemToAdd: any, partitionKey: string, rowKey: string): Promise<any>;
+  private getStorageAccount();
 }
 
